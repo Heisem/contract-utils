29a99621499645e3d50dde560b8c9b55dbb3bba7
Carlos Parra <carlos.parra@nearshoresystems.com>
2018-04-19 13:09:50 -0300

    moved to es6 inheritance due to front end issues

--- packages/transaction/index.js
+++ packages/transaction/index.js
@@ -1,41 +1,42 @@
 const ethereumTx = require('ethereumjs-tx');
 const Web3 = require('web3');
 
-// Contract Utility Library
-class Transaction extends Web3 {
-  /**
-   * Create ContractUtils
-   * @param {string} providerUrl provider url
-   * @param {object} contracts example: { 'erc20': { address: '', abi: [] } }
-   */
-  constructor(providerUrl, contracts) {
-    super(providerUrl);
-    this.contracts = contracts;
-  }
+/**
+ * Create ContractUtils
+ * @param {string} providerUrl provider url
+ * @param {object} contracts example: { 'erc20': { address: '', abi: [] } }
+ */
+const Transaction = function (providerUrl, contracts) {
+  Web3.call(this, providerUrl);
+  this.contracts = contracts; 
+}
 
-  /**
-   * Create a Custom Contract Object
-   * @param {string} contractName
-   * @returns {object} contract
-   */
-  createContract(contractName) {
-    const contract = this.contracts[contractName];
-    const { abi, address } = contract;
-    return {
-      contractName,
-      address,
-      contract: new this.eth.Contract(abi, address),
-    };
+Transaction.prototype = Object.create(Web3.prototype);
+Transaction.prototype.constructor = Transaction;
+
+/**
+ * Create a Custom Contract Object
+ * @param {string} contractName
+ * @returns {object} contract
+ */
+Transaction.prototype.create = function createContract(contractName) {
+  const contract = this.contracts[contractName];
+  const { abi, address } = contract;
+  return {
+    contractName,
+    address,
+    contract: new this.eth.Contract(abi, address),
   };
+};
 
-  /**
-   * Create a Raw Transaction Object
-   * @param {contract} contract contract
-   * @param {string} from from address
-   * @param {object} params params as array
-   * @returns {object} rawTx
-   */
-  async create(contract, gasLimit = 200000, value = 0, method, fromAddress, params) {
+/**
+ * Create a Raw Transaction Object
+ * @param {contract} contract contract
+ * @param {string} from from address
+ * @param {object} params params as array
+ * @returns {object} rawTx
+ */
+Transaction.prototype.create = async function createContract(contract, gasLimit = 200000, value = 0, method, fromAddress, params) {
     const nonce = await this.eth.getTransactionCount(fromAddress);
     const gasPriceGwei = await this.eth.getGasPrice();
     return {
@@ -50,37 +51,36 @@ class Transaction extends Web3 {
     };
   };
 
-  /**
-   * Sign a Raw Transaction
-   * @param {string} privateKey 
-   * @param {object} rawTransaction
-   * @returns {object} signedTransaction
-   */
-  sign(privateKey, rawTransaction) {
-    const key = new Buffer(privateKey, 'hex');
-    const tx = new ethereumTx(rawTransaction);
-    tx.sign(key);
-    const serializedTx = tx.serialize();
-    return `0x${serializedTx.toString('hex')}`;
-  }
-
-  /**
-   * Send Raw Transsaction
-   * @param {string} contractName // contract name defined in './contract/index'
-   * @param {string} method // contract function to be called
-   * @param {string} privateKey // private key
-   * @param {object} params // params as defined in the contract function
-   * @returns {object} receipt from transaction
-   */
-  async send({ contractName, privateKey, gasLimit, value }, method, ...params) {
-    const contract = this.createContract(contractName);
-    const account = this.eth.accounts.privateKeyToAccount(`0x${privateKey}`);
-    const fromAddress = account.address;
-    const rawTransaction = await this.create(contract, gasLimit, value, method, fromAddress, params);
-    const signedTransaction = this.sign(privateKey, rawTransaction);
-    const receipt = await this.eth.sendSignedTransaction(signedTransaction);
-    return receipt;
-  };
+/**
+ * Sign a Raw Transaction
+ * @param {string} privateKey 
+ * @param {object} rawTransaction
+ * @returns {object} signedTransaction
+ */
+Transaction.prototype.sign = function signTransaction(privateKey, rawTransaction) {
+  const key = new Buffer(privateKey, 'hex');
+  const tx = new ethereumTx(rawTransaction);
+  tx.sign(key);
+  const serializedTx = tx.serialize();
+  return `0x${serializedTx.toString('hex')}`;
 }
+
+/**
+ * Send Raw Transsaction
+ * @param {string} contractName // contract name defined in './contract/index'
+ * @param {string} method // contract function to be called
+ * @param {string} privateKey // private key
+ * @param {object} params // params as defined in the contract function
+ * @returns {object} receipt from transaction
+ */
+Transaction.prototype.send = async function sendTransaction({ contractName, privateKey, gasLimit, value }, method, ...params) {
+  const contract = this.createContract(contractName);
+  const account = this.eth.accounts.privateKeyToAccount(`0x${privateKey}`);
+  const fromAddress = account.address;
+  const rawTransaction = await this.create(contract, gasLimit, value, method, fromAddress, params);
+  const signedTransaction = this.sign(privateKey, rawTransaction);
+  const receipt = await this.eth.sendSignedTransaction(signedTransaction);
+  return receipt;
+};
 
 module.exports = Transaction;
